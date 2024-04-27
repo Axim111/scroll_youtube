60548af035a41993f00de14955262c97b6ef65a0
Axim <makseropolov@gmail.com>
2024-04-27 23:30:25 +0500

    scroll youtube v 0.1

--- index.js
+++ index.js
@@ -12,21 +12,16 @@ await page.goto(link);
 
 console.log(await page.evaluate('window.innerWidth'))
 
-
-// await page.evaluate(() => {
-//   window.scrollTo(0, 50000000);
-// });
-
 let prevHeight = -1;
 let maxScrolls = 100;
 let scrollCount = 0;
 
 while (scrollCount < maxScrolls) {
-  // Scroll to the bottom of the page
+
   await page.evaluate('window.scrollTo(0, document.documentElement.scrollHeight)');
-  // Wait for page load
+
   await new Promise(resolve => setTimeout(resolve, 1000));
-  // Calculate new scroll height and compare
+
   let newHeight = await page.evaluate('document.documentElement.scrollHeight');
   if (newHeight == prevHeight) {
     break;
@@ -35,13 +30,7 @@ while (scrollCount < maxScrolls) {
   scrollCount += 1;
 }
 
-
 let items = await page.$$("#video-title > .ytd-video-renderer")
-const sleep = () => new Promise((resolve, reject) => {
-
-
-})
-
 
 for (let item of items) {
   console.log(await item.evaluate((node) =>
